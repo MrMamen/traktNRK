5c7704242e199e4366c2a9fb5b03ae8f995060ec
MrMamen <erikandre.mamen@gmail.com>
2019-10-12 21:53:50 +0200

    Cleaned up item-mapping

--- app/scripts/src/content/item.js
+++ app/scripts/src/content/item.js
@@ -3,25 +3,16 @@
 /* A way to fix translation issues with NRK-titles and trakt-titles */
 const tvdbIDs = {
   "narvestad-tar-ferie": 337638,
-  "solgt": 278940,
   "broen": 252019,
-  "kampen-om-tungtvannet": 289393,
   "laserblikk-paa-historien": 278057,
   "monster": 334378,
-  "ukens-vinner": 306065,
-  "side-om-side": 272955,
-  "verdens-toeffeste-togturer": 264771,
-  "paa-benken": 333947,
-  "over-hekken": 299939,
-  "hva-feiler-det-dege": 320101,
   "kalde-foetter": 77152,
   "le-bureau": 294564,
-  "stacey-dooley-forbudt-sex": 308622,
   "hotellenes-hemmeligheter": 264122,
-  "gutter-er-gutter": 269593,
   "ei-tidsreise-gjennom-science-fiction-si-historie": 279432,
   "lykkeland": 344464,
-  "romarriket": 343856
+  "romarriket": 343856,
+  "exit": 361816
 };
 
 function Item(options) {
